710b22de2f0ff347a6ac2c6784e4254e5109bc9b
nikita <nikita@MacBook-Pro-4.local>
2021-04-02 02:58:01 +0300

    upd

--- src/components/Space/SpaceCreate/AddArtobjects/AddArtobjects.js
+++ src/components/Space/SpaceCreate/AddArtobjects/AddArtobjects.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
+import './styles.css'
 
 export default class AddArtobjects extends Component {
     render() {
-        const { num } = this.props;
+        const { num, hoveredWall, hoveredArtobject, hoverWall, unhoverWall, hoverArtobject, unhoverArtobject } = this.props;
         return (
             <div className={'create-add-artobjects'}>
-                <div className={'create-add-artobjects-title'}>Wall {num}</div>
-                <div className={'create-add-artobjects-cont'}>
-                    <div className={'create-add-artobjects-add'} id={1 + 3 * (num - 1)}></div>
-                    <div className={'create-add-artobjects-add'} id={2 + 3 * (num - 1)}></div>
-                    <div className={'create-add-artobjects-add'} id={3 + 3 * (num - 1)}></div>
+                <div className={num > 2 ? 'create-add-artobjects-order' : ''}>
+                    <div className={'create-add-artobjects-title'}>Wall {num}</div>
+                    <div 
+                        className={`create-add-artobjects-cont ${hoveredWall == num ? 'create-add-artobjects-cont-hover' : ''}`} 
+                        id={`w${num}`}
+                        onMouseEnter={hoverWall}
+                        onMouseLeave={unhoverWall}
+                    >
+                        <div className={'create-add-artobjects-add'} id={1 + 3 * (num - 1)} onMouseEnter={hoverArtobject} onMouseLeave={unhoverArtobject}></div>
+                        <div className={'create-add-artobjects-add'} id={2 + 3 * (num - 1)} onMouseEnter={hoverArtobject} onMouseLeave={unhoverArtobject}></div>
+                        <div className={'create-add-artobjects-add'} id={3 + 3 * (num - 1)} onMouseEnter={hoverArtobject} onMouseLeave={unhoverArtobject}></div>
+                    </div>
+                </div>
+                <div className={num > 2 ? 'create-add-artobjects-order-3d' : ''}>
+                    <div className={'create-add-artobjects-title create-add-artobjects-title-3d'}>3D object {num}</div>
+                    <div className={'create-add-artobjects-add-3d'} id={12 + num} onMouseEnter={hoverArtobject} onMouseLeave={unhoverArtobject}></div>
                 </div>
             </div>
         )
--- src/components/Space/SpaceCreate/SpaceCreate.js
+++ src/components/Space/SpaceCreate/SpaceCreate.js
@@ -7,24 +7,37 @@ import './styles.css'
 
 class SpaceCreate extends Component {
     state = {
-        showWelcome: false
+        showWelcome: false,
+        hoveredWall: 0,
+        hoveredArtobject: 0
     }
 
     closeWelcome = () => this.setState({showWelcome: false})
 
+    hoverWall = e => this.setState({hoveredWall: e.target.id[2] || e.target.id[1]})
+    unhoverWall = () => this.setState({hoveredWall: 0})
+
+    hoverArtobject = e => this.setState({hoveredArtobject: e.target.id})
+    unhoverArtobject = () => this.setState({hoveredArtobject: 0})
+
     render() {
-        const { showWelcome } = this.state;
+        const { showWelcome, hoveredWall, hoveredArtobject } = this.state;
         return (
             <div className={'body-cont'}>
                 <div className={'create-cont'}>
                     <div className={'create-cont-top'}>
-                        <AddArtobjects num={1} />
-                        <AddArtobjects num={2} />
+                        <AddArtobjects num={1} hoveredWall={hoveredWall} hoverWall={this.hoverWall} unhoverWall={this.unhoverWall} hoverArtobject={this.hoverArtobject} unhoverArtobject={this.unhoverArtobject}/>
+                        <AddArtobjects num={2} hoveredWall={hoveredWall} hoverWall={this.hoverWall} unhoverWall={this.unhoverWall} hoverArtobject={this.hoverArtobject} unhoverArtobject={this.unhoverArtobject}/>
                     </div>
-                    <Wall />
+                    <div className={'space-create-wall-cont'}>
+                        <Wall num={1} hoveredWall={hoveredWall} hoveredArtobject={hoveredArtobject} hoverWall={this.hoverWall} unhoverWall={this.unhoverWall}/>
+                        <Wall num={2} hoveredWall={hoveredWall} hoveredArtobject={hoveredArtobject} hoverWall={this.hoverWall} unhoverWall={this.unhoverWall}/>
+                        <Wall num={4} hoveredWall={hoveredWall} hoveredArtobject={hoveredArtobject} hoverWall={this.hoverWall} unhoverWall={this.unhoverWall}/>
+                        <Wall num={3} hoveredWall={hoveredWall} hoveredArtobject={hoveredArtobject} hoverWall={this.hoverWall} unhoverWall={this.unhoverWall}/>
+                    </div>
                     <div className={'create-cont-bottom'}>
-                        <AddArtobjects num={3} />
-                        <AddArtobjects num={4} />
+                        <AddArtobjects num={4} hoveredWall={hoveredWall} hoveredArtobject={hoveredArtobject} hoverWall={this.hoverWall} unhoverWall={this.unhoverWall} hoverArtobject={this.hoverArtobject} unhoverArtobject={this.unhoverArtobject}/>
+                        <AddArtobjects num={3} hoveredWall={hoveredWall} hoveredArtobject={hoveredArtobject} hoverWall={this.hoverWall} unhoverWall={this.unhoverWall} hoverArtobject={this.hoverArtobject} unhoverArtobject={this.unhoverArtobject}/>
                     </div>
                 </div>
                 <div className={'create-btn-cont'}>
