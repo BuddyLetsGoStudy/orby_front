fb42cfb66f30afea57afe8727b5acbd0439dd2c2
nikita <nikita@MacBook-Pro-4.local>
2021-05-30 02:57:35 +0300

    upd

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,7 @@ import './styles.css'
 
 const Button = props => {
     const { size = 'big', color = 'blue', arrow = false, fontSize = '18px', margin = '0', onClick, text } = props;
-    return <div className={`button button-${size} button-${color} ${arrow ? 'button-arrow' : ''}`} style={{fontSize, margin}} onClick={onClick}>{text}</div>
+    return <div className={`button button-${size} ${arrow ? 'button-arrow' : ''}`} style={{fontSize, margin}} onClick={onClick}><div className={`button-${color} button-colored`}/><div className={`button-${color}-hovered button-colored`}/><div className={'button-text'}>{text}</div></div>
 }
 
 export default Button
