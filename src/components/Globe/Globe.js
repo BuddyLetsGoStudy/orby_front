08970fe6152f0a91425d2ef161f0dce2f645acde
nikita <nikita@MacBook-Pro-4.local>
2021-08-05 00:41:59 +0300

    upd

--- src/components/Globe/Globe.js
+++ src/components/Globe/Globe.js
@@ -1,33 +1,61 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { AnimatePresence, motion } from "framer-motion"
+import { pageAnimation } from '../../variables'
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Globe = () => {
-    useEffect(() => {
-        const script = document.createElement('script');
-      
-        script.src = "https://api.orby.space/media/common/api.js";
-        script.async = true;
-      
-        document.body.appendChild(script);
-      
-        return () => {
-          document.body.removeChild(script);
-        }
-      }, []);
-      
-    return (
-        <>
-          <div id="earth_div"></div>
-          <div className="globe-plus-cont">
+  const [ hint, changeHint ] = useState(false)
+  const [ plus, changePlus ] = useState(false)
+
+  useEffect(() => {
+      const script = document.createElement('script');
+    
+      script.src = "https://api.orby.space/media/common/api.js";
+      script.async = true;
+    
+      document.body.appendChild(script);
+
+      setTimeout(() => changePlus(true), 4000)
+      setTimeout(() => changeHint(true), 5000)
+    
+      return () => {
+        document.body.removeChild(script);
+      }
+    }, []);
+  
+  
+  return (
+      <>
+        <div id="earth_div"></div>
+        <AnimatePresence exitBeforeEnter>
+          {
+            plus &&
+            <motion.div className="globe-plus-cont" initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
             <div className="globe-plus-bg">
-              <div className="globe-plus">
+              <AnimatePresence exitBeforeEnter>
+                {
+                    hint &&  
+                    <motion.div className={"globe-plus-hint"} initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}>
+                        <div className={"globe-plus-hint-text"}>Hey! Let’s create your own space 🙂</div>
+                        <div className={"globe-plus-hint-close"} onClick={() => changeHint(false)}/>
+                    </motion.div>
+                }
+              </AnimatePresence>
+              <Link to="/create" className="globe-plus">
                 <div className="globe-plus-icon"/>
-              </div>
+              </Link>
+              <motion.div className="globe-plus-bg-glow" initial={{opacity: 0}} animate={{opacity: 1, transition: {duration: 2, yoyo: Infinity}}} exit={{opacity: 0}}/>
+
             </div>
-          </div>
-        </>
-    )
+            </motion.div>
+          }
+      </AnimatePresence>
+         
+
+      </>
+  )
 }
 
 export default Globe
